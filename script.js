7ea525f4830107a3a45252b390a9c1827d2699fb
Akib-Hasaan <akibhasan582@gmail.com>
2024-11-11 22:44:00 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// (2) Map
+let w = ["alu", "bhaji"]
+w.map(plate=>{console.log(plate);})
+
+
+// (3) ForEach
+let z = ["alu", "bhorta"]
+z.map(bowl=>{console.log(bowl);})
+
+// **Conclusion** The same result can be provided by 3 different "methods" (for, forEach & map) 
+// **Note** Always use "map", because "forEach" allocates more memory
